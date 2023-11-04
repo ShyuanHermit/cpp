b38328452cb162e921fb85242f2be7ed8a69e37e
ShyuanHermit <Shyuan231026nwpu@outlook.com>
2023-11-04 20:09:10 +0800

    dfgh

--- noj/noj_1-10/noj 5.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int mum;
-	cin>>mum;
-	printf("%X,%o",mum,mum);
-return 0;
-}
--- 试做.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-int main(){ 
-char a;
-scanf("%c",&a);//(如果是C) 
- printf("%o\n",a);//o指八进制 
- printf("%d\n",a);//d指十进制 
- printf("%x\n",a);//x指十六进制 
-	return 0;
-}
