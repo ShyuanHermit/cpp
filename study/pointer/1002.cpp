c89c523cefa6eb5cd9c3a61865c1e17a73bed167
ShyuanHermit <Shyuan231026nwpu@outlook.com>
2023-11-17 16:51:07 +0800

    cpp

---
+++ study/pointer/1002.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: /home/lvshy/cpp/study/pointer/1002.cpp
+    > Author: lvshy
+    > Mail: Shyuan231026nwpu@outlook.com 
+    > Created Time: Thu Nov 16 20:23:39 2023
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+return 0;
+}
+//这次我们来讲讲数组指针。
