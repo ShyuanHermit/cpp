a944774fdf5c6eff2c7432b34063a478309fd0b0
ShyuanHermit <Shyuan231026nwpu@outlook.com>
2023-11-20 22:39:44 +0800

    cpp

--- noj/noj_31-40/noj-38.cpp
+++ noj/noj_31-40/noj-38.cpp
@@ -6,59 +6,71 @@
  ************************************************************************/
 
 #include<bits/stdc++.h>
-
 using namespace std;
+#define PI 3.1415926
 double VolaCalc(double r){
-	return 4*M_PI*r*r*r/3;}
+	return r*r*r/3*PI*4;}
 double VolaCalc(double r,double h){
-	return M_PI*r*r*h/3;}
+	return PI*r*r*h/3;}
 double VolaCalc(long a){return a*a*a;
 	}
-double VolaCalc(double r,long h){return M_PI*r*r*h;
+double VolaCalc(double r,long h){return PI*r*r*h;
 	}
 double VolaCalc(long l,long w,long h){return l*w*h;
 	}
 double VolaCalc(double r,double R,double h){
-return M_PI*h*h*(3*R-h)/3;}//
+return PI*h*h*(3*R-h)/3;}//
 double VolaCalc(double r,double R,double h,double l){
-	return 2*VolaCalc(r,R,h)+M_PI*r*r*l;}//
+	return 2*VolaCalc(r,R,h)+PI*r*r*l;}//
 double VolaCalc(long r,double R,double h){
-	return M_PI*(r*r+R*R+R*r)*h/3;}
+	return PI*(r*r+R*R+R*r)*h/3;}
 double VolaCalc(double a,double b,long c){
-	return 4*M_PI*a*b*c/3;}
+	return 4*PI*a*b*c/3;}
 double VolaCalc(long a,long h){return a*a*h/3;
 	}
 double VolaCalc(long d1,long d2,double l){
-	return l*M_PI*(d1*d1-d2*d2)/4;}
+	return l*PI*(d1*d1-d2*d2)/4;}
 int main(){
 	
-	double r1;cin>>r1;
 
-	double r2,h2;cin>>r2>>h2;	
+	int N,i=1;cin>>N;
+cout<<"1";	
+double r1;cin>>r1;
 
-	long a3;cin>>a3;
+cout<<"2"; 
+double r2,h2;cin>>r2>>h2;	
 
-	double r4;long h4;cin>>r4>>h4;
+cout<<"3"; 
+long a3;cin>>a3;
 
-	long l5,w5,h5;cin>>l5>>w5>>h5;
+cout<<"4"; 
+double r4;long h4;cin>>r4>>h4;
 
-	double r6,R6,h6,h6_;cin>>r6>>R6;
-	h6=R6+sqrt(R6*R6-r6*r6);
-	h6_=R6-sqrt(R6*R6-r6*r6);
+cout<<"5"; 
+long l5,w5,h5;cin>>l5>>w5>>h5;
 
-	double r7,R7,h7,h7_,l7;cin>>r7>>R7>>l7;
+cout<<"6"; 
+double r7,R7,h7,h7_,l7;cin>>r7>>R7>>l7;
 	h7=R7+sqrt(R7*R7-r7*r7);
 	h7_=R7-sqrt(R7*R7-r7*r7);
 
-	long r8;double R8,h8;cin>>r8>>R8>>h8;
+cout<<"7"; 
+double r6,R6,h6,h6_;cin>>r6>>R6;
+	h6=R6+sqrt(R6*R6-r6*r6);
+	h6_=R6-sqrt(R6*R6-r6*r6);
 
-	double a9,b9;long c9;cin>>a9>>b9>>c9;
+cout<<"8"; 
+long r8;double R8,h8;cin>>r8>>R8>>h8;
 
-	long a10,h10;cin>>a10>>h10;
+cout<<"9"; 
+double a9,b9;long c9;cin>>a9>>b9>>c9;
 
-	long d1,d2;double l11;cin>>d1>>d2>>l11;
+cout<<"10"; 
+long a10,h10;cin>>a10>>h10;
+
+cout<<"11"; 
+long d1,d2;double l11;cin>>d1>>d2>>l11;
 	
-
  printf("%.6f\n" , VolaCalc(r1) );
  
  printf("%.6f\n" ,VolaCalc(r2,h2) );
@@ -69,11 +81,11 @@ int main(){
  
  printf("%.6f\n" ,VolaCalc(l5,w5,h5) );
  
- printf("%.6f\t" ,VolaCalc(r6,R6,h6) );
-	 printf("%.6f\n" ,VolaCalc(r6,R6,h6_) );
- 
- printf("%.6f\t" ,VolaCalc(r7,R7,h7,l7) );
+ printf("%.6f " ,VolaCalc(r7,R7,h7,l7) );
 	 printf("%.6f\n" ,VolaCalc(r7,R7,h7_,l7) );
+ 
+ printf("%.6f " ,VolaCalc(r6,R6,h6) );
+	 printf("%.6f\n" ,VolaCalc(r6,R6,h6_) );
  
  printf("%.6f\n" ,VolaCalc(r8,R8,h8) );
  
@@ -82,7 +94,6 @@ int main(){
  printf("%.6f\n" ,VolaCalc(a10,h10) );
  
  printf("%.6f\n" ,VolaCalc(d1,d2,l11) );
-
 
 return 0;
 }
